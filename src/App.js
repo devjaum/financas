6590f5be153b6ddc72836caa2ea7f2be195db6d2
João Carlos <devjaum@gmail.com>
2025-12-20 22:16:44 -0300

    1.0

--- src/App.js
+++ src/App.js
@@ -1,11 +1,8 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import LoginComponent from './components/LoginComponent.tsx';
 import {ROUTES} from "./app.router.ts";
 import './App.css';
-import HomeComponent from './components/HomeComponent.tsx';
 import Dashboard from './components/Dashboard.tsx';
-import SetupAccount from './components/SetupAccount.tsx';
-import AddTransaction from './components/AddTransaction.tsx';
+
 import { ThemeProvider } from './contexts/ThemeContext.tsx';
 
 function App() {
@@ -13,11 +10,9 @@ function App() {
     <ThemeProvider>
       <BrowserRouter>
         <Routes>
-          <Route path={ROUTES.LOGIN} element={<LoginComponent />} />
-          <Route path="/home/:id" element={<HomeComponent />} />
+
           <Route path={ROUTES.DASHBOARD} element={<Dashboard />} />
-          <Route path={ROUTES.SETUP_ACCOUNT} element={<SetupAccount />} />
-          <Route path={ROUTES.ADD_TRANSACTION} element={<AddTransaction />} />
+
         </Routes>
       </BrowserRouter>
     </ThemeProvider>
--- src/app.router.ts
+++ src/app.router.ts
@@ -1,10 +1,6 @@
 //Como faço um router no react?
 export const ROUTES = {
-    LOGIN: "/login",
-    HOME: "/home",
     DASHBOARD: "/",
-    SETUP_ACCOUNT: "/setup",
-    ADD_TRANSACTION: "/add-transaction",
 } as const;
 
 export type RouteKey = keyof typeof ROUTES;
